36ea6c748e3dc5d750d16e44377e9413717a3333
Bram Laurijsen <gizmo_xxl@hotmail.com>
2021-06-28 15:51:23 +0200

    new excercises

---
+++ my-svelte-project/src/EachBlocks.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let cats = [
+		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+	];
+</script>
+
+<h3>The Famous Cats of YouTube</h3>
+
+<ul>
+	{#each cats as cat}
+		<li><a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
+			{cat.name}
+		</a></li>
+	{/each}
+</ul>
+
+<ul>
+	{#each cats as { id, name }, i}
+		<li><a target="_blank" href="https://www.youtube.com/watch?v={id}">
+			{i + 1}: {name}
+		</a></li>
+	{/each}
+</ul>
+
